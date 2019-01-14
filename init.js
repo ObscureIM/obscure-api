243a6b6ee17346bca5b5cd7956075d5067f846ec
Syahrul <pauscrypto@gmail.com>
2019-01-14 21:53:13 +0800

    Added the express server to accept CORS

--- init.js
+++ init.js
@@ -6,6 +6,13 @@ var app = express();
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
+    app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
   })
 
 
